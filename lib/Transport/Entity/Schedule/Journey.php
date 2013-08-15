b57bd3d4e1788716b82012fd3a2b5d0b420af195
Roman Prokofyev <roman.prokofyev@gmail.com>
2013-08-15 11:50:15 +0200

    more Basel improvements

--- lib/Transport/Entity/Schedule/Journey.php
+++ lib/Transport/Entity/Schedule/Journey.php
@@ -9,7 +9,7 @@ class Journey
     array('BaselJourney' => array('places' => array('Basel', 'Pratteln', 'Rodersdorf', 'Dornach-Arlesheim', 'Ettingen',
                                                     'Flüh', 'Basel Wiesenplatz', 'Birsfelden', 'Allschwil', 'Riehen',
                                                     'Hüslimatt', 'Aesch', 'Aesch BL Dorf', 'St-Louis Grenze',
-                                                    'Binningen', 'Basel Messeplatz'),
+                                                    'Binningen', 'Basel Messeplatz', 'Basel Bankverein', 'Oberwil BL'),
                                   'colors' => array('1' => 'ff804b2f', '2' => 'ffa8834a', '3' => 'ff3c4796',
                                                     '6' => 'ff006ab0', '8' => 'ffeb6ea2', '10' => 'ffffca0a',
                                                     '11' => 'ffe30910', '14' => 'ffeb7b05', '15' => 'ff00963a',
