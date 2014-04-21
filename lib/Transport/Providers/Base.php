e95e028b04df5c72d1319cc572497b47365f9184
Roman Prokofyev <roman.prokofyev@gmail.com>
2014-04-21 17:25:40 +0200

    initial params

---
+++ lib/Transport/Providers/Base.php
@@ -0,0 +1,8 @@
+class API
+{
+    protected const REQ_PROD = 'hafas';
+    protected const API_VERSION = '1.1';
+
+    protected const SEARCH_MODE_NORMAL = 'N';
+    protected const SEARCH_MODE_ECONOMIC = 'P';
+}
