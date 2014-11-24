ee0366bce636e951f71358f18a3a0f22b656a422
Roman Prokofyev <roman.prokofyev@gmail.com>
2014-11-24 19:11:23 +0100

    finished sms tickets import

--- mongo_import/scripts/import_sms_tickets.py
+++ mongo_import/scripts/import_sms_tickets.py
@@ -9,7 +9,6 @@ import unicodedata
 def main_import(srv_addr, db_name):
     print "Converting to json"
     f = open('data/sms_tickets.csv')
-    out = open('temp_import/sms_tickets.json', 'w')
     reader = csv.reader(f)
 
     headers = reader.next()
@@ -29,3 +28,17 @@ def main_import(srv_addr, db_name):
             sms_location = location
             sms_locations[sms_number] = sms_location
         sms_location['localities'].append({'name': location.pop('locality'), 'zone': location.pop('zone')})
+
+
+    print "Importing into MongoDB at %s/%s" % (srv_addr, db_name)
+    from pymongo import MongoClient
+    import pymongo
+    client = MongoClient(srv_addr)
+    db = client[db_name]
+    if 'sms_tickets' in db.collection_names():
+        db.drop_collection('sms_tickets')
+
+    db.sms_tickets.insert(sms_locations.values())
+    print "Creating indexes"
+    db.sms_tickets.ensure_index('localities.name')
+    client.close()
