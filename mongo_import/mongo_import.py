de0a9fad3ebb6a628c38aa2694737a81b1d14ace
Roman Prokofyev <roman.prokofyev@gmail.com>
2015-01-01 14:20:41 +0100

    implemeting line colors database

--- mongo_import/mongo_import.py
+++ mongo_import/mongo_import.py
@@ -4,10 +4,13 @@
 import sys
 SRV_ADDR, DB_NAME = sys.argv[1:]
 
-from scripts import import_sms_tickets, import_stations
+from scripts import import_sms_tickets, import_stations, import_line_colors
 
 print "Importing stations"
 import_stations.main_import(SRV_ADDR, DB_NAME)
+
+print "Importing line color codes"
+import_line_colors.main_import(SRV_ADDR, DB_NAME)
 
 print "Importing SMS ticket codes"
 import_sms_tickets.main_import(SRV_ADDR, DB_NAME)
---
+++ mongo_import/scripts/import_line_colors.py
@@ -0,0 +1,45 @@
+import csv
+import json
+import subprocess
+import os
+import re
+import unicodedata
+
+
+def main_import(srv_addr, db_name):
+    print "Converting to json"
+    f = open('data/line_colors.csv')
+    reader = csv.reader(f)
+
+    headers = reader.next()
+    locations = {}
+
+    for row in reader:
+        line = {}
+        for key, value in zip(headers, row):
+            line[key] = value
+        if not line:
+            continue
+        location_name = line['location']
+        if location_name in locations:
+            location = locations[location_name]
+        else:
+            location = line
+            location['location'] = {'type': 'Point', 'coordinates': [float(location.pop('location_lon')), float(location.pop('location_lat')) ]}
+            location['lines'] = []
+            locations[location_name] = location
+        location['lines'].append({line.pop('line_num'): line.pop('color')})
+
+
+    print "Importing into MongoDB at %s/%s" % (srv_addr, db_name)
+    from pymongo import MongoClient
+    import pymongo
+    client = MongoClient(srv_addr)
+    db = client[db_name]
+    if 'line_colors' in db.collection_names():
+        db.drop_collection('line_colors')
+
+    db.line_colors.insert(locations.values())
+    print "Creating indexes"
+    db.line_colors.ensure_index([("location", pymongo.GEOSPHERE)])
+    client.close()
